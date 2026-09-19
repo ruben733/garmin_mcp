388c26dc45fadfc06f5bf150b28b1007bdb90408
Ruben Martinez <rubencivic@hotmail.com>
2026-09-19 16:35:13 +0200

    Fix Garmin diagnostic indentation

--- src/garmin_mcp/__init__.py
+++ src/garmin_mcp/__init__.py
@@ -433,7 +433,7 @@ def init_api(email, password):
         finally:
             sys.stderr = old_stderr
 
-except (FileNotFoundError, GarminConnectConnectionError, GarminConnectTooManyRequestsError, GarminConnectAuthenticationError) as err:
+    except (FileNotFoundError, GarminConnectConnectionError, GarminConnectTooManyRequestsError, GarminConnectAuthenticationError) as err:
         print(f"GARMIN LOGIN DIAGNOSTIC: {type(err).__name__}", file=sys.stderr)
         # Session is expired. You'll need to log in again
 
